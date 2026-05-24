6d5f5e74b705c9a6179906602e2fce00c060c859
younseolee <younseo6376@g.skku.edu>
2026-05-24 14:14:35 +0900

    Create app.py

---
+++ app.py
@@ -0,0 +1,36 @@
+import streamlit as st
+
+# 1. 제목 적기
+st.title("Why Luxury Brands Invest in BLACKPINK")
+
+# 2. 이미지 넣기 (이미지 파일명을 그대로 적으세요)
+# 파일이 루트 폴더에 흩어져 있다고 하셨으니 파일명만 적으면 바로 나옵니다.
+st.image("jennie1.webp", caption="멧갈라 속 제니 메인 이미지")
+st.image("jennie2.webp", caption="멧갈라 속 제니 전신 이미지")
+st.image("jennie3.webp", caption="멧갈라 속 제니 얼굴 포커스 이미지")
+st.image("jisoo1.webp", caption="멧갈라 속 지수 메인 이미지")
+st.image("jisoo2.webp", caption="멧갈라 속 지수 전신 이미지")
+st.image("jisoo3.webp", caption="멧갈라 속 지수 얼굴 포커스 이미지")
+st.image("rose1.webp", caption="멧갈라 속 로제 메인 이미지")
+st.image("rose2.webp", caption="멧갈라 속 로제 전신 이미지")
+st.image("rose3.webp", caption="멧갈라 속 로제 얼굴 포커스 이미지")
+st.image("lisa1.webp", caption="멧갈라 속 리사 메인 이미지")
+st.image("lisa2.webp", caption="멧갈라 속 리사 전신 이미지")
+st.image("lisa3.webp", caption="멧갈라 속 리사 얼굴 포커스 이미지")
+
+st.image("chanel1.jpeg", caption="샤넬 공식 인스타 멧갈라 게시물1")
+st.image("chanel2.jpg", caption="샤넬 공식 인스타 멧갈라 게시물2")
+st.image("chanel3.jpg", caption="샤넬 공식 인스타 멧갈라 게시물3")
+st.image("dior1.jpg", caption="디올 공식 인스타 멧갈라 게시물1")
+st.image("dior2.jpg", caption="디올 공식 인스타 멧갈라 게시물2")
+st.image("dior3.jpg", caption="디올 공식 인스타 멧갈라 게시물3")
+st.image("dior4.jpg", caption="디올 공식 인스타 멧갈라 게시물4")
+st.image("dior5.jpg", caption="디올 공식 인스타 멧갈라 게시물5")
+st.image("dior6.jpg", caption="디올 공식 인스타 멧갈라 게시물6")
+st.image("yls1.jpg", caption="생로랑 공식 인스타 멧갈라 게시물4")
+st.image("yls2.jpg", caption="생로랑 공식 인스타 멧갈라 게시물5")
+st.image("yls3.jpg", caption="생로랑 공식 인스타 멧갈라 게시물6")
+
+
+# 3. 추가 텍스트 적기
+st.write("Focusing on the Met Gala attendance case, it will be organized in the direction of visualizing why luxury brands are actively investing in BLACKPINK. The core goal is not just to introduce fashion, but to analyze how BLACKPINK affects the expansion of a brand's global awareness and digital engagement based on data. In particular, we want to show that BLACKPINK is functioning as a key marketing asset that amplifies brand value and buzz beyond simple celebrities through indicators such as SNS response, media exposure, and brand mention. Accordingly, it was intended to create an archived website that allows working-level MD and brand officials to intuitively compare and analyze Met Gala cases and quickly organize notes and insights. In addition, it aims to effectively convey the connection between fashion and digital marketing by organizing the styling strategy and digital ripple effect of each brand at a glance..")
